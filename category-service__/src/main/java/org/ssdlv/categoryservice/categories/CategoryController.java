2fac989c6456e63edce232de40ab1bc607606a3f
marielevha <mspeedy733@gmail.com>
2021-07-12 13:20:58 +0200

    first commit service-suppliers project

--- category-service__/src/main/java/org/ssdlv/categoryservice/categories/CategoryController.java
+++
@@ -1,7 +0,0 @@
-package org.ssdlv.categoryservice.categories;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CategoryController {
-}
--- category-service__/src/main/java/org/ssdlv/categoryservice/categories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ssdlv.categoryservice.categories;
-
-//import org.springframework.data.jpa.repository.JpaRepository;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CategoryRepository extends MongoRepository<Category, Long> {
-}
